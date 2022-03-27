b8ee5bf39a4a7ccab4812cdf915adeafc11630c7
Chase <chaseroldan1@gmail.com>
2022-03-27 14:50:09 -0700

    edit

--- README.md
+++
@@ -1,1 +0,0 @@
-# studiolabnftmarketplace.github.io
---
+++ data.js
@@ -0,0 +1,40 @@
+const data = [{
+  "id": 1,
+  "name": "Jacket",
+  "image": "./resources/images/jacket.jpeg",
+  "desc": "Jacket",
+  "price": 25
+}, {
+  "id": 2,
+  "name": 'T-Shirt',
+  "image": './resources/images/tshirt.jpg',
+  "desc": "T-Shirt",
+  "price": 20
+}, {
+  "id": 3,
+  "name": 'Tank Top',
+  "image": "./resources/images/apparel.jpg",
+  "desc": "Tank Top",
+  "price": 15
+}, {
+  "id": 3,
+  "name": 'Hat',
+  "image": "./resources/images/hat.jpg",
+  "desc": "Hat",
+  "price": 15
+}, {
+  "id": 5,
+  "name": 'Album',
+  "image": "./resources/images/album.jpg",
+  "desc": "Album",
+  "price": 10
+}, {
+  "id": 9,
+  "name": 'Accessory',
+  "image": "./resources/images/accessories.jpg",
+  "desc": "Accessory",
+  "price": 5
+}, 
+]
+
+export default data;
